63dc07a29e4195d0e8899b8fb775c99d389d8949
Paris Kontoulis <pkontoulis@isc.tuc.gr>
2021-06-01 00:10:00 +0300

    Update config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   // authAPI: "https://game.kontoulis.me/auth/",
   // playAPI: "https://game.kontoulis.me/play/",
   // gameAPI: "https://game.kontoulis.me/game/",
-  authAPI: "http://127.0.0.1:3001",
-  playAPI: "http://127.0.0.1:3002",
-  gameAPI: "http://127.0.0.1:3003",
+  authAPI: "http://paris.kontoulis.me:3001",
+  playAPI: "http://paris.kontoulis.me:3002",
+  gameAPI: "http://paris.kontoulis.me:3003",
 };
